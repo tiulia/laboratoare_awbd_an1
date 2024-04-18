fba02349eb80205d596ca9b951eaca0fadaef090
tiulia <th_iulia84@yahoo.com>
2024-04-18 08:07:43 +0300

    lab 8

--- LAB8_Mongo/mongo-seed/import_data.sh
+++
@@ -1,3 +0,0 @@
-mongoimport --host mongo_test --username umovies --password pmovies --db moviesdb --collection movies --type json --jsonArray --file /movies.json
-mongoimport --host mongo_test --username umovies --password pmovies --db moviesdb --collection embedded_movies --type json --jsonArray --file /embedded_movies.json
-mongoimport --host mongo_test --username umovies --password pmovies --db moviesdb --collection comments --type json --jsonArray --file /comments.json
